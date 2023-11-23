eb6228e04c31463aaa931cef81fb96df64d7ade8
Ukondy <dimapotrebic@gmail.com>
2023-11-23 19:54:17 +0300

    add Assured solution 1

---
+++ src/test/java/api/UserData.java
@@ -0,0 +1,17 @@
+package api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserData {
+    private Integer id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private String avatar;
+
+}
